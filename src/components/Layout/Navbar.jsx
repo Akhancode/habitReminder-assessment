13cc09df87fbe3b82b8cef2bca877ec612995bea
akhancode <akhan.code@gmail.com>
2024-11-02 16:28:37 +0530

    Finished almost

--- src/components/Layout/Navbar.jsx
+++ src/components/Layout/Navbar.jsx
@@ -25,35 +25,35 @@ const Navbar = () => {
 
   const currentPath = location.pathname;
   let LeftDiv = <div>
-    <h3 onClick={redirectToHome} className='font-semibold text-2xl text-white'>Habits</h3>
+    <h3 onClick={redirectToHome} className=' cursor-pointer font-semibold text-2xl text-white'>Habits</h3>
   </div>
-  let RightDiv = <div className='flex items-center justify-end gap-3'>
-    <button onClick={gotoAddPage} className='flex items-center hover:text-gray-400 active:text-gray-700 cursor-pointer justify-center  font-semibold text-4xl text-white'>+</button>
-    <button onClick={redirectToDashBoard} className='flex items-center hover:text-gray-400 active:text-gray-700 cursor-pointer justify-center  font-semibold text-4xl text-white'>📈</button>
+  let RightDiv = <div className='cursor-pointer flex items-center justify-end gap-3'>
+    <button onClick={gotoAddPage} className='flex  items-center hover:text-gray-400 active:text-gray-700 cursor-pointer justify-center  font-semibold text-4xl text-white'>+</button>
+    <button onClick={redirectToDashBoard} className=' flex items-center hover:text-gray-400 active:text-gray-700 cursor-pointer justify-center  font-semibold text-4xl text-white'>📈</button>
   </div>
 
   if (currentPath.startsWith('/habit')) {
-    RightDiv = <div className='flex items-center justify-end'>
+    RightDiv = <div className='flex cursor-pointer  items-center justify-end'>
       <button onClick={redirectToHome} className='flex items-center hover:text-gray-400 hover:border-gray-400 active:text-gray-700 cursor-pointer justify-center  font-semibold text-xl text-white border rounded-md px-4 py-1 '>cancel</button>
     </div>
   }
   if (currentPath.startsWith('/dashboard')) {
     LeftDiv = <div>
-      <h3 onClick={redirectToHome} className='font-semibold text-2xl text-white'>🔙</h3>
+      <h3 onClick={redirectToHome} className='cursor-pointer  font-semibold text-2xl text-white'>🔙</h3>
     </div>
-    RightDiv = <div className='flex items-center justify-end gap-3'>
+    RightDiv = <div className='cursor-pointer  flex items-center justify-end gap-3'>
     </div>
   }
   if (currentPath.startsWith('/progress')) {
 
     LeftDiv = <div>
-      <h3 onClick={redirectToHome} className='font-semibold text-2xl text-white'>🔙</h3>
+      <h3 onClick={redirectToHome} className='cursor-pointer  font-semibold text-2xl text-white'>🔙</h3>
     </div>
-    RightDiv = <div onClick={() => (redirectToEdit(habitId))} className='flex items-center justify-end gap-3'>✏️
+    RightDiv = <div onClick={() => (redirectToEdit(habitId))} className='flex cursor-pointer  items-center justify-end gap-3'>✏️
     </div>
   }
   return (
-    <div className=' bg-[#424242] min-h-20 max-h-28 flex justify-between items-center px-6'>
+    <div className=' cursor-pointer bg-[#424242] min-h-20 max-h-28 flex justify-between items-center px-6'>
       {LeftDiv}
       {RightDiv}
     </div>
